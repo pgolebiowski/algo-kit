3ea1b55f3b5220c50b9b836f021471b695ecd634
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2016-07-22 21:30:13 +0200

    Project setup

---
+++ src/AlgoKit/AlgoKit.Test/ToBeDeletedTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace AlgoKit.Test
+{
+    [TestFixture]
+    public class ToBeDeletedTests
+    {
+        [Test]
+        [TestCase(true)]
+        [TestCase(false)]
+        public void ShouldReturnSameValue(bool input)
+        {
+            var instance = new ToBeDeleted();
+            Assert.AreEqual(input, instance.Fixed(input));
+        }
+    }
+}
---
+++ src/AlgoKit/AlgoKit.TestConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoKit.TestConsole
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("AlgoKit Test Console");
+            Console.WriteLine("Doing stuff...");
+
+            var instance = new ToBeDeleted();
+            Console.WriteLine(instance.Fixed(true));
+
+            Console.WriteLine();
+            Console.WriteLine("Finished");
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
